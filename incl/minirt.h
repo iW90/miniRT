307abd8766ee92b05c686b49f69c3b48352bf276
Marcos <marcos.chiu@gmail.com>
2023-11-17 21:35:57 -0300

    feat: added allowed libraries

--- incl/minirt.h
+++ incl/minirt.h
@@ -6,18 +6,20 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 20:52:10 by maalexan          #+#    #+#             */
-/*   Updated: 2023/11/17 21:10:52 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/11/17 21:26:45 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-# include "mlx/MLX42/MLX42.h"
-# include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include "mlx/MLX42/MLX42.h"
 
 # define WIN_HEIGHT 600
-# define WIN_WIDHT 800
+# define WIN_WIDTH 800
 
 #endif
