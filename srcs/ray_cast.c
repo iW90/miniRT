fe644cc045347c2f6fe3bb26d20b01a703d951df
Marcos <marcos.chiu@gmail.com>
2024-02-22 13:21:38 -0300

    feat: added function comments

--- srcs/ray_cast.c
+++ srcs/ray_cast.c
@@ -6,23 +6,59 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 15:14:37 by maalexan          #+#    #+#             */
-/*   Updated: 2024/02/20 19:51:00 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:21:11 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_vec3f	cast_ray(t_vec3f orig, t_vec3f dir, t_ambient ambient)
+/*
+** Casts a ray into the scene and determines the color seen along that ray, 
+** based on intersections with spheres.
+** The color calculation takes into account ambient lighting and the color 
+** of the intersected sphere.
+**
+** Parameters:
+** - orig: The origin of the ray.
+** - dir: The direction of the ray.
+** - spheres: A linked list of spheres in the scene.
+** - amb: The ambient lighting, defined by its intensity and color.
+**
+** Returns:
+** - The color seen along the ray as a vector, where each component 
+** represents the red, green, and blue color channels.
+**
+** The function iterates through the list of spheres, using the intersect 
+** function to find the closest sphere (if any) that intersects with the ray. 
+** If an intersection is found, the color of the sphere is scaled by the ambient 
+** light intensity to calculate the hit color. This hit color is then combined 
+** with the ambient light color to determine the final color seen in the 
+** direction of the ray. If no intersection is found, the resulting color
+** is solely determined by the ambient light.
+*/
+t_vec3f	cast_ray(t_vec3f orig, t_vec3f dir, t_sphere *spheres, t_ambient amb)
 {
-	t_vec3f	ambient_color;
-	t_vec3f	final_color;
-	/* Ray-object intersection logic
-	*/
+	float		t;
+	float		closest_t;
+	t_vec3f		hit_color;
+	t_vec3f		ambient_color;
+	t_sphere	*closest_sphere;
 
-	// Apply ambient lighting to the hit color
-	// Note: You might want to normalize ambient.rgb values to [0, 1] if they are initially [0, 255]
-	ambient_color = vec3f_scale(ambient.rgb, ambient.ratio);
-	final_color = vec3f_add(hit_color, ambient_color); //hit color is ray object logic
-
-	return (final_color);
+	hit_color = (t_vec3f){0};
+	closest_t = INFINITY;
+	closest_sphere = NULL;
+	while (spheres)
+	{
+		t = INFINITY;
+		if (intersect((t_ray){orig, dir}, spheres, &t) && t < closest_t)
+		{
+			closest_t = t;
+			closest_sphere = spheres;
+		}
+		spheres = spheres->next;
+	}
+	if (closest_sphere)
+		hit_color = vec3f_scale(closest_sphere->rgb, amb.ratio);
+	ambient_color = vec3f_scale(amb.rgb, amb.ratio);
+	return (vec3f_add(hit_color, ambient_color));
 }
--- srcs/sphere.c
+++ srcs/sphere.c
@@ -6,12 +6,30 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 22:18:15 by maalexan          #+#    #+#             */
-/*   Updated: 2024/02/22 11:45:23 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/02/22 13:21:20 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/*
+** Solves a quadratic equation of the form ax^2 + bx + c = 0.
+** The coefficients of the equation are passed as a vector, where
+** coeffs.x represents 'a', coeffs.y represents 'b', and coeffs.z represents 'c'.
+** The roots of the equation, if existants, are returned as pointers t0 and t1.
+**
+** Parameters:
+** - coeffs: A vector containing the coefficients of the quadratic equation.
+** - t0: A pointer to a float where the first root will be stored.
+** - t1: A pointer to a float where the second root will be stored.
+**
+** Returns:
+** - TRUE if the equation has real roots, FALSE otherwise.
+**
+** The function first calculates the discriminant to check if the roots are real.
+** If real roots exist, it computes them using the quadratic formula. 
+** The roots are then sorted to ensure t0 <= t1.
+*/
 t_bool	solve_quadratic(t_vec3f coeffs, float *t0, float *t1)
 {
 	float	q;
@@ -36,6 +54,30 @@ t_bool	solve_quadratic(t_vec3f coeffs, float *t0, float *t1)
 	return (TRUE);
 }
 
+/*
+** Determines if a ray intersects with a given sphere 
+** and calculates the intersection distance.
+** Uses the analytical solution to the ray-sphere intersection problem.
+**
+** Parameters:
+** - ray: The ray being cast, defined by an origin and a direction.
+** - sphere: The sphere to test for intersection,
+** defined by its center coordinates and diameter.
+** - t: A pointer to a float where the distance from the ray origin
+** to the intersection point will be stored.
+**
+** Returns:
+** - TRUE if the ray intersects with the sphere, FALSE otherwise.
+**
+** The function first converts the sphere's diameter to a radius
+** for the intersection calculation.
+** It then computes the coefficients for the quadratic equation 
+** that arises from plugging the ray equation into the sphere's equation.
+** These coefficients are used in the solve_quadratic function to find 
+** potential intersection points. The function ensures that the intersection
+** point is the closest one in the direction of the ray and discards 
+** intersections behind the ray origin.
+*/
 t_bool	intersect(t_ray ray, t_sphere sphere, float *t)
 {
 	float	t0;
