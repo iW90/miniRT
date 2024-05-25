aa3764fa2c472151e5003ac715f706e098222f60
Marcos <marcos.chiu@gmail.com>
2024-05-25 12:29:22 -0300

    feat: header

--- srcs/util_print_header.c
+++ srcs/util_print_header.c
@@ -6,25 +6,24 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 00:14:31 by maalexan          #+#    #+#             */
-/*   Updated: 2024/05/24 13:53:38 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/05/25 12:29:03 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static void	print_first_header(void)
+void	print_first_header(void)
 {
-	printf(COLOR_PL);
+	printf(COLOR_GR_1);
 	printf("   •  •     \n");
 	printf("┏┳┓┓┏┓┓  ┏┓╋\n");
 	printf("┛┗┗┗┛┗┗  ┛ ┗\n");
 	printf("\n");
 }
 
-static void	print_second_header(void)
+void	print_second_header(void)
 {
 	printf(COLOR_WH);
-	printf("HEADER2\n\n");
 	printf("███╗   ███╗██╗███╗   ██╗██╗    ██████╗ ████████╗\n");
 	printf("████╗ ████║██║████╗  ██║██║    ██╔══██╗╚══██╔══╝\n");
 	printf("██╔████╔██║██║██╔██╗ ██║██║    ██████╔╝   ██║   \n");
@@ -34,9 +33,8 @@ static void	print_second_header(void)
 	printf("\n");
 }
 
-static void	print_third_header(void)
+void	print_third_header(void)
 {
-	printf("HEADER3\n\n");
 	printf(COLOR_RE);
 	printf("███▄ ▄███▓ ██▓ ███▄    █  ██▓    ██▀███  ▄▄▄█████▓\n");
 	printf("▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▓██▒   ▓██ ▒ ██▒▓  ██▒ ▓▒\n");
@@ -52,9 +50,10 @@ static void	print_third_header(void)
 void	print_header(void)
 {
 	printf("\n");
-	print_first_header();
-	print_second_header();
-	print_third_header();
+	if (DEBUG)
+		print_third_header();
+	else
+		print_first_header();
 	printf(COLOR_NO);
 	printf("\n");
 }
